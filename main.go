b35bc2ae9461ac4393a2995efb925e9321fab70f
mh-cbon <mh-cbon@users.noreply.github.com>
2016-06-15 22:15:24 +0200

    go fmt, go vet

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
-  "strconv"
-  "path/filepath"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
-  "github.com/mh-cbon/archive/uncompress"
-  "github.com/jhoonb/archivex"
-  "github.com/dustin/go-humanize"
-  "github.com/urfave/cli"
+	"github.com/dustin/go-humanize"
+	"github.com/jhoonb/archivex"
+	"github.com/mh-cbon/archive/uncompress"
+	"github.com/urfave/cli"
 )
 
-var VERSION ="0.0.0"
+var VERSION = "0.0.0"
 
-func main () {
-  app := cli.NewApp()
+func main() {
+	app := cli.NewApp()
 	app.Name = "archive"
 	app.Version = VERSION
 	app.Usage = "Command line to create and extract archive files"
@@ -58,121 +58,121 @@ func main () {
 		},
 	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
 
-func create (c *cli.Context) error {
-  output := c.String("output")
-  files := make([]string, 0)
-  for _, f := range c.Args() {
-    sF := string(f)
-    _, err := os.Stat(f);
-    if !os.IsNotExist(err) {
-      files = append(files, sF)
-    } else {
-      fmt.Println("File '"+sF+"' does not exist")
-    }
-  }
-  if len(output)==0 {
-    return cli.NewExitError("Output argument is required", 1)
-  }
-  if len(files)==0 {
-    return cli.NewExitError("<file> arguments to archive are missing, or provided paths does not exists.", 1)
-  }
-  if _, err := os.Stat(output); !os.IsNotExist(err) {
-    if c.Bool("force")==false {
-      return cli.NewExitError("Output file already exists. Use force argument.", 1)
-    }
-  }
+func create(c *cli.Context) error {
+	output := c.String("output")
+	files := make([]string, 0)
+	for _, f := range c.Args() {
+		sF := string(f)
+		_, err := os.Stat(f)
+		if !os.IsNotExist(err) {
+			files = append(files, sF)
+		} else {
+			fmt.Println("File '" + sF + "' does not exist")
+		}
+	}
+	if len(output) == 0 {
+		return cli.NewExitError("Output argument is required", 1)
+	}
+	if len(files) == 0 {
+		return cli.NewExitError("<file> arguments to archive are missing, or provided paths does not exists.", 1)
+	}
+	if _, err := os.Stat(output); !os.IsNotExist(err) {
+		if c.Bool("force") == false {
+			return cli.NewExitError("Output file already exists. Use force argument.", 1)
+		}
+	}
 
-  var archiver archivex.Archivex
-  if strings.Index(output, ".zip")>-1 {
-    archiver = new(archivex.ZipFile)
-  } else if strings.Index(output, ".tar")>-1 {
-    archiver = new(archivex.TarFile)
-  } else if strings.Index(output, ".tgz")>-1 {
-    output = strings.Replace(output, filepath.Ext(output), "", -1) + ".tar.gz"
-    archiver = new(archivex.TarFile)
-  } else {
-    return cli.NewExitError("Cannot handle archive '"+output+"' is not zip|tar|tar.gz.", 1)
-  }
+	var archiver archivex.Archivex
+	if strings.Index(output, ".zip") > -1 {
+		archiver = new(archivex.ZipFile)
+	} else if strings.Index(output, ".tar") > -1 {
+		archiver = new(archivex.TarFile)
+	} else if strings.Index(output, ".tgz") > -1 {
+		output = strings.Replace(output, filepath.Ext(output), "", -1) + ".tar.gz"
+		archiver = new(archivex.TarFile)
+	} else {
+		return cli.NewExitError("Cannot handle archive '"+output+"' is not zip|tar|tar.gz.", 1)
+	}
 
-  dir := filepath.Dir(output)
-  if len(dir)>0 {
-    err := os.MkdirAll(dir, 0755)
-    if err!= nil {
-      return cli.NewExitError("Failed to create output directory '"+dir+"'.", 1)
-    }
-  }
+	dir := filepath.Dir(output)
+	if len(dir) > 0 {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return cli.NewExitError("Failed to create output directory '"+dir+"'.", 1)
+		}
+	}
 
-  err := archiver.Create(output)
-  if err!= nil {
-    fmt.Println(err)
-    return cli.NewExitError("Failed to create output archive '"+output+"'.", 1)
-  }
-  gotErr := false
-  for _, f := range files {
-    s, _ := os.Stat(f);
-    if s.IsDir() {
-      err = archiver.AddAll(f, true)
-      if err!= nil {
-        fmt.Println(err)
-        gotErr = true
-      }
-    } else {
-      err = archiver.AddFile(f)
-      if err!= nil {
-        fmt.Println(err)
-        gotErr = true
-      }
-    }
-  }
-  err = archiver.Close()
-  if err!= nil {
-    fmt.Println(err)
-    gotErr = true
-  }
-  if gotErr {
-    os.Remove(output)
-    return cli.NewExitError("Failed to create the archive '"+output+"'.", 1)
-  }
-  s, _ := os.Stat(output);
-  fmt.Println("✓ "+output+ ": "+humanize.Bytes(uint64(s.Size())))
-  return nil
+	err := archiver.Create(output)
+	if err != nil {
+		fmt.Println(err)
+		return cli.NewExitError("Failed to create output archive '"+output+"'.", 1)
+	}
+	gotErr := false
+	for _, f := range files {
+		s, _ := os.Stat(f)
+		if s.IsDir() {
+			err = archiver.AddAll(f, true)
+			if err != nil {
+				fmt.Println(err)
+				gotErr = true
+			}
+		} else {
+			err = archiver.AddFile(f)
+			if err != nil {
+				fmt.Println(err)
+				gotErr = true
+			}
+		}
+	}
+	err = archiver.Close()
+	if err != nil {
+		fmt.Println(err)
+		gotErr = true
+	}
+	if gotErr {
+		os.Remove(output)
+		return cli.NewExitError("Failed to create the archive '"+output+"'.", 1)
+	}
+	s, _ := os.Stat(output)
+	fmt.Println("✓ " + output + ": " + humanize.Bytes(uint64(s.Size())))
+	return nil
 }
 
-func extract (c *cli.Context) error {
-  dest := c.String("dest")
-  src := string(c.Args().Get(0))
-  if len(dest)==0 {
-    wd, err := os.Getwd()
-    if err!=nil {
-      return cli.NewExitError("Cannot determine destination path", 1)
-    }
-    dest = wd
-  }
-  if _, err := os.Stat(dest); !os.IsNotExist(err) {
-    if c.Bool("force")==false {
-      return cli.NewExitError("Destination already exists. Use force argument.", 1)
-    }
-  }
-  if len(src)==0 {
-    return cli.NewExitError("<file> source arguments are required.", 1)
-  }
-  info := make(chan string)
-  itemsCnt := 0
-  go func (){
-    for f := range info {
-      fmt.Println(f)
-      itemsCnt++
-    }
-  }()
-  err := uncompress.Uncompress(src, dest, info)
-  close(info)
-  if err!= nil {
-    fmt.Println(err)
-    return cli.NewExitError("Failed to extract the archive '"+src+"'.", 1)
-  }
-  fmt.Println("✓ "+dest+ ": "+strconv.Itoa(itemsCnt)+" files created")
-  return nil
+func extract(c *cli.Context) error {
+	dest := c.String("dest")
+	src := string(c.Args().Get(0))
+	if len(dest) == 0 {
+		wd, err := os.Getwd()
+		if err != nil {
+			return cli.NewExitError("Cannot determine destination path", 1)
+		}
+		dest = wd
+	}
+	if _, err := os.Stat(dest); !os.IsNotExist(err) {
+		if c.Bool("force") == false {
+			return cli.NewExitError("Destination already exists. Use force argument.", 1)
+		}
+	}
+	if len(src) == 0 {
+		return cli.NewExitError("<file> source arguments are required.", 1)
+	}
+	info := make(chan string)
+	itemsCnt := 0
+	go func() {
+		for f := range info {
+			fmt.Println(f)
+			itemsCnt++
+		}
+	}()
+	err := uncompress.Uncompress(src, dest, info)
+	close(info)
+	if err != nil {
+		fmt.Println(err)
+		return cli.NewExitError("Failed to extract the archive '"+src+"'.", 1)
+	}
+	fmt.Println("✓ " + dest + ": " + strconv.Itoa(itemsCnt) + " files created")
+	return nil
 }
--- uncompress/uncompress.go
+++ uncompress/uncompress.go
@@ -1,229 +1,226 @@
 package uncompress
 
 import (
-  "os"
-  "io"
-  "strings"
-  "path/filepath"
-  "errors"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 
-  "compress/gzip"
-	"archive/zip"
 	"archive/tar"
+	"archive/zip"
+	"compress/gzip"
 )
 
-func Uncompress (src string, dest string, info chan<- string) error {
-  if filepath.Ext(src)==".zip" {
-    return Unzip(src, dest, info)
-  } else if filepath.Ext(src)==".gz" && strings.Index(src, ".tar.gz")>-1 {
-    return Untargz(src, dest, info)
-  } else if filepath.Ext(src)==".tgz" {
-    return Untargz(src, dest, info)
-  } else if filepath.Ext(src)==".tar" {
-    return Untar(src, dest, info)
-  } else {
-    return errors.New("cannot handle file '"+src+"'")
-  }
+func Uncompress(src string, dest string, info chan<- string) error {
+	if filepath.Ext(src) == ".zip" {
+		return Unzip(src, dest, info)
+	} else if filepath.Ext(src) == ".gz" && strings.Index(src, ".tar.gz") > -1 {
+		return Untargz(src, dest, info)
+	} else if filepath.Ext(src) == ".tgz" {
+		return Untargz(src, dest, info)
+	} else if filepath.Ext(src) == ".tar" {
+		return Untar(src, dest, info)
+	} else {
+		return errors.New("cannot handle file '" + src + "'")
+	}
 }
 
 func Unzip(src, dest string, info chan<- string) error {
-  r, err := zip.OpenReader(src)
-  if err != nil {
-    return err
-  }
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
 
-  os.MkdirAll(dest, 0755)
+	os.MkdirAll(dest, 0755)
 
-  // Closure to address file descriptors issue with all the deferred .Close() methods
-  extractAndWriteFile := func(srcFile *zip.File) error {
-    rc, err := srcFile.Open()
-    if err != nil {
-      return err
-    }
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(srcFile *zip.File) error {
+		rc, err := srcFile.Open()
+		if err != nil {
+			return err
+		}
 
-    path := filepath.Join(dest, srcFile.Name)
+		path := filepath.Join(dest, srcFile.Name)
 
-    if srcFile.FileInfo().IsDir() {
-      info <- path
-      return os.MkdirAll(path, srcFile.Mode())
-    } else {
-      info <- path
-      err = os.MkdirAll(filepath.Dir(path), 0755)
-      if err != nil {
-        return err
-      }
-      dstFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, srcFile.Mode())
-      if err != nil {
-        return err
-      }
-      _, err = io.Copy(dstFile, rc)
-      if err != nil {
-        dstFile.Close();
-        return err
-      }
-      return dstFile.Close()
-    }
-    return nil
-  }
+		if srcFile.FileInfo().IsDir() {
+			info <- path
+			return os.MkdirAll(path, srcFile.Mode())
+		} else {
+			info <- path
+			err = os.MkdirAll(filepath.Dir(path), 0755)
+			if err != nil {
+				return err
+			}
+			dstFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, srcFile.Mode())
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(dstFile, rc)
+			if err != nil {
+				dstFile.Close()
+				return err
+			}
+			return dstFile.Close()
+		}
+	}
 
-  for _, f := range r.File {
-    err := extractAndWriteFile(f)
-    if err != nil {
-      r.Close();
-      return err
-    }
-  }
-  err = r.Close();
-  if err != nil {
-    return err
-  }
-  return nil
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			r.Close()
+			return err
+		}
+	}
+	err = r.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func Untar(src, dest string, info chan<- string) error {
-  srcFile, err := os.Open(src)
+	srcFile, err := os.Open(src)
 	if err != nil {
-    return err
+		return err
 	}
 
 	tarReader := tar.NewReader(srcFile)
 	if err != nil {
 		srcFile.Close()
-    return err
+		return err
 	}
 
-  os.MkdirAll(dest, 0755)
+	os.MkdirAll(dest, 0755)
 
-  extractAndWriteFile := func(src *tar.Reader, hdr *tar.Header) error {
+	extractAndWriteFile := func(src *tar.Reader, hdr *tar.Header) error {
 
-    path := filepath.Join(dest, hdr.Name)
-    stat := hdr.FileInfo()
+		path := filepath.Join(dest, hdr.Name)
+		stat := hdr.FileInfo()
 
-    if stat.IsDir() {
-      info <- path
-      err = os.MkdirAll(path, 0755)
-      if err != nil {
-        return err
-      }
-      return nil;
-    } else {
-      info <- path
-      err = os.MkdirAll(filepath.Dir(path), 0755)
-      if err != nil {
-        return err
-      }
-      dst, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
-      if err != nil {
-        return err
-      }
+		if stat.IsDir() {
+			info <- path
+			err = os.MkdirAll(path, 0755)
+			if err != nil {
+				return err
+			}
+			return nil
+		} else {
+			info <- path
+			err = os.MkdirAll(filepath.Dir(path), 0755)
+			if err != nil {
+				return err
+			}
+			dst, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+			if err != nil {
+				return err
+			}
 
-      _, err = io.Copy(dst, src)
-      if err != nil {
-        dst.Close();
-        return err
-      }
-      return dst.Close()
-    }
-    return nil
-  }
+			_, err = io.Copy(dst, src)
+			if err != nil {
+				dst.Close()
+				return err
+			}
+			return dst.Close()
+		}
+	}
 
-  for {
+	for {
 		hdr, err := tarReader.Next()
 		if err == io.EOF {
 			// end of tar archive
 			break
 		}
 		if err != nil {
-  		srcFile.Close()
+			srcFile.Close()
 			return err
 		}
-    err = extractAndWriteFile(tarReader, hdr)
-    if err != nil {
-  		srcFile.Close()
-      return err
-    }
+		err = extractAndWriteFile(tarReader, hdr)
+		if err != nil {
+			srcFile.Close()
+			return err
+		}
 	}
 
-  return srcFile.Close()
+	return srcFile.Close()
 }
 
 func Untargz(src, dest string, info chan<- string) error {
-  srcFile, err := os.Open(src)
+	srcFile, err := os.Open(src)
 	if err != nil {
-    return err
+		return err
 	}
 
 	gzf, err := gzip.NewReader(srcFile)
 	if err != nil {
 		srcFile.Close()
-    return err
+		return err
 	}
 
 	tarReader := tar.NewReader(gzf)
 	if err != nil {
 		gzf.Close()
 		srcFile.Close()
-    return err
+		return err
 	}
 
-  os.MkdirAll(dest, 0755)
+	os.MkdirAll(dest, 0755)
 
-  extractAndWriteFile := func(src *tar.Reader, hdr *tar.Header) error {
+	extractAndWriteFile := func(src *tar.Reader, hdr *tar.Header) error {
 
-    path := filepath.Join(dest, hdr.Name)
-    stat := hdr.FileInfo()
+		path := filepath.Join(dest, hdr.Name)
+		stat := hdr.FileInfo()
 
-    if stat.IsDir() {
-      info <- path
-      err = os.MkdirAll(path, 0755)
-      if err != nil {
-        return err
-      }
-      return nil;
-    } else {
-      info <- path
-      err = os.MkdirAll(filepath.Dir(path), 0755)
-      if err != nil {
-        return err
-      }
-      dst, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, stat.Mode())
-      if err != nil {
-        return err
-      }
+		if stat.IsDir() {
+			info <- path
+			err = os.MkdirAll(path, 0755)
+			if err != nil {
+				return err
+			}
+			return nil
+		} else {
+			info <- path
+			err = os.MkdirAll(filepath.Dir(path), 0755)
+			if err != nil {
+				return err
+			}
+			dst, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, stat.Mode())
+			if err != nil {
+				return err
+			}
 
-      _, err = io.Copy(dst, src)
-      if err != nil {
-        dst.Close();
-        return err
-      }
-      return dst.Close()
-    }
-    return nil
-  }
+			_, err = io.Copy(dst, src)
+			if err != nil {
+				dst.Close()
+				return err
+			}
+			return dst.Close()
+		}
+	}
 
-  for {
+	for {
 		hdr, err := tarReader.Next()
 		if err == io.EOF {
 			// end of tar archive
 			break
 		}
 		if err != nil {
-  		gzf.Close()
-  		srcFile.Close()
+			gzf.Close()
+			srcFile.Close()
 			return err
 		}
-    err = extractAndWriteFile(tarReader, hdr)
-    if err != nil {
-  		gzf.Close()
-  		srcFile.Close()
-      return err
-    }
+		err = extractAndWriteFile(tarReader, hdr)
+		if err != nil {
+			gzf.Close()
+			srcFile.Close()
+			return err
+		}
 	}
 
-  err = gzf.Close();
-  if err != nil {
-    srcFile.Close()
-    return err
-  }
-  return srcFile.Close()
+	err = gzf.Close()
+	if err != nil {
+		srcFile.Close()
+		return err
+	}
+	return srcFile.Close()
 }
